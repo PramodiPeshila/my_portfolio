3607573622690408e6b127d3b2c9fe18dd2ce764
Pramodi Peshila <ppeshila7@gmail.com>
2025-08-07 15:52:24 +0530

    Modified file

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import React, { useEffect, useRef, useState } from "react";
 
 const Navbar = () => {
-  
+
   const [isScroll,setIsScroll] = useState(false);
   const sideMenuRef = useRef();
 
@@ -29,15 +29,16 @@ const Navbar = () => {
 
   return (
     <>
-      <div className="fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]">
+      <div className="fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%] hidden">
         <Image src={assets.header_bg_color} alt=" " className="w-full " />
       </div>
-      <nav className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50 transition-all duration-300 ${isScroll ? "bg-white/50 backdrop-blur-sm shadow-lg navbar-blur" : ""}`}>
+      <nav className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50  ${isScroll ? "backdrop-blur-lg shadow-sm bg-darkTheme/80 shadow-white/20" : ""}`}>
         <a href="#top">
-          <Image src={assets.logo} alt="logo" className="w-28 cursor-pointer mr-14"/>
+          {/* <Image src={assets.logo_dark} alt="logo" className="w-28 cursor-pointer mr-14" /> */}
+           <h2 className="text-3xl font-bold font-Ovo mb-2 text-white">Pramodi Peshila</h2>
         </a>
               
-        <ul className={`hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 ${isScroll ? "" : "bg-white shadow-sm bg-opacity-50"}`}>
+        <ul className={`hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 ${isScroll ? "" : "border border-white/50 bg-transparent"}`}>
           <li><a className='font-Ovo' href="#top">Home</a></li>
           <li><a className='font-Ovo' href="#about">About me</a></li>
           <li><a className='font-Ovo' href="#services">Services</a></li>
@@ -46,24 +47,20 @@ const Navbar = () => {
         </ul>
 
         <div className="flex items-center gap-4">
-          
-          <button>
-            <Image src={assets.moon_icon} alt=" " className="w-6" />
-           </button>
 
-          <a href="#contact" className='hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 font-Ovo'>
-            Contact <Image src={assets.arrow_icon}  alt="" className="w-3" />
+          <a href="#contact" className='hidden lg:flex items-center gap-3 px-10 py-2.5 border border-white/50 rounded-full ml-4 font-Ovo'>
+            Contact <Image src={assets.arrow_icon_dark} alt="" className="w-3" />
           </a>
           <button className="block md:hidden ml-3" onClick={openMenu}>
-            <Image src={assets.menu_black} alt=" " className="w-6" />
+            <Image src={assets.menu_white} alt=" " className="w-6" />
           </button>
         </div>
         
         {/* Mobile menu */}
-         <ul ref ={sideMenuRef} className="flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0w-64 z-50 h-screen bg-rose-50 transition duration-500">
+        <ul ref={sideMenuRef} className="flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-[#11001F] text-white transition duration-500">
           
           <div className="absolute right-6 top-6" onClick={closeMenu}>
-            <Image src={assets.close_black} alt=" " className="w-5 cursor-pointer " />
+            <Image src={assets.close_white} alt=" " className="w-5 cursor-pointer " />
           </div>
 
           <li><a className='font-Ovo' onClick={closeMenu} href="#top">Home</a></li>
