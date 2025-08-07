6e0eeda81b19c37e56fe65150f7cc4a513d6fa5e
Pramodi Peshila <ppeshila7@gmail.com>
2025-08-07 08:35:35 +0530

    Added Navigation to services,work and contact sections

--- app/page.js
+++ app/page.js
@@ -2,6 +2,9 @@
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import About from "./components/About";
+import Services from "./components/Services";  
+import Work from "./components/Work"; 
+import Contact from "./components/Contact";  
 
 export default function Home() {
   return (
@@ -9,6 +12,9 @@ export default function Home() {
       <Navbar />
       <Header />
       <About />
+      <Services />
+      <Work />
+      <Contact />
     </>
   );
 }
