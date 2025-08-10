b3f5ffe6711e1e96cf48c6c5ad17d56edd105a2c
Pramodi Peshila <ppeshila7@gmail.com>
2025-08-10 20:17:35 +0530

    Updated file to deploy in the github

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export', // forces static export for GitHub Pages
+  images: {
+    unoptimized: true, // prevents image optimization errors during static export
+  },
+  basePath: process.env.NODE_ENV === 'production' ? '/my_portfolio' : '', // repo name in production only
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/my_portfolio/' : '',
+};
 
 export default nextConfig;
